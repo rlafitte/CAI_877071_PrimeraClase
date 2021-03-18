0a087034673bd138dd3d7e339969d643ae6800b3
Renard <rdlafitte@gmail.com>
2021-03-17 22:09:52 -0300

    Calculadora
    Se crea lógica para ingreso de datos de usuario. La librería de clases tiene los métodos mediante los cuales se validan los ingresos, mientras que program.cs tiene el programa principal.

--- PrimeraClase/PrimeraClase.Interfaz/Program.cs
+++ PrimeraClase/PrimeraClase.Interfaz/Program.cs
@@ -11,6 +11,41 @@ namespace PrimeraClase.Interfaz
     {
         static void Main(string[] args)
         {
+            //cada proyecto tiene su propio assembly
+            //sólo uno tiene un ejecutable, cada solución tiene un sólo ejecutable
+
+            //para linkear los proyectos, tengo que usar las referencias en el solution explorer
+            // la referencia sólo se debe crear en el proyecto que tenga el .exe, salvo que creemos más de un proyecto
+
+            //Tomar variables (nro A, nro B, tipo operacion)
+            // validaciones (TIENEN QUE ESTAR EN EL OTRO PROYECTO)
+            // Procesar esas variables en un archivo que este en el otro proyecto
+            // Consola viva, el programa no se puede terminar a menos que el usuario ingrese una X
+
+            string s_nro1 = "";
+            string s_nro2 = "";
+            string s_ope = "";
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese un número:");
+                s_nro1 = Console.ReadLine();
+            } while (!Class1.ValidaNumero(s_nro1));
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese otro número:");
+                s_nro2 = Console.ReadLine();
+            } while (!Class1.ValidaNumero(s_nro2));
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese un operador:");
+                s_ope = Console.ReadLine();
+
+            } while (!Class1.ValidaOperador(s_ope));
+
+            Console.ReadKey();
         }
     }
 }
--- PrimeraClase/PrimeraClase.LibreriaClases/Class1.cs
+++ PrimeraClase/PrimeraClase.LibreriaClases/Class1.cs
@@ -8,5 +8,32 @@ namespace PrimeraClase.LibreriaClases
 {
     public class Class1
     {
+        public static bool Longitud(string palabra, int longitudMaxima)
+        {
+            //Método que valida si excede longitud
+
+            return (palabra.Length >= longitudMaxima);
+
+        }
+
+        public static bool ValidaNumero(string valor)
+        {
+            // valida que se ingresó un número
+            int value = 0;
+            return (int.TryParse(valor, out value));
+        }
+
+        public static bool ValidaOperador(string valor)
+        {
+            bool flag = false;
+            string operadores = "+-*/";
+            //test
+
+            if (string.Compare(valor, operadores) >= 0)
+            {
+                flag = true;
+            }
+            return flag;
+        }
     }
 }
